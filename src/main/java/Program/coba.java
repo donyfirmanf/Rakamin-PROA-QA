df247736628b9696b5da7bb3b000f947516bfaf3
Dony Firman Fajariza <123849730+donyfirmanf@users.noreply.github.com>
2023-10-21 09:28:07 +0700

    Add files via upload

---
+++ src/main/java/Program/coba.java
@@ -0,0 +1,18 @@
+package Program;
+
+import javax.swing.*;
+
+public class coba
+{
+    public static void main (String []arg)
+    {
+        //ini adalah program untuk mencetak hello word
+        System.out.println("hello, World");
+        System.out.println("hello, medicahyono");
+        System.out.println("hello, kholifah");
+        System.out.println("hello, taqy");
+
+        JOptionPane.showMessageDialog(null,"Hay Medic, Kholif dan Taqy");
+        JOptionPane.showConfirmDialog(null,"kamu yakin mau lanjut ?");
+    }
+}
